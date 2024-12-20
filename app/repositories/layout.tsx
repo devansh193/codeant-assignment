3d7d28226ae8642da4250e1ca51e283671d6f6b0
devansh <devanshverma193@gmail.com>
2024-12-20 12:44:59 +0530

    fix

--- app/repositories/layout.tsx
+++ app/repositories/layout.tsx
@@ -3,7 +3,7 @@ import { DashSidebar } from "./_components/sidebar/sidebar";
 const DashLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-[#FFFFFF] flex h-screen">
-      <div className="">
+      <div>
         <DashSidebar />
       </div>
       <div className="md:ml-[250px] w-full">{children}</div>
