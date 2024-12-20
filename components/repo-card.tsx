18ca1ba8b0b711df8770d9f06d2e024fc38b9871
devansh <devanshverma193@gmail.com>
2024-12-20 17:40:55 +0530

    fix

--- components/repo-card.tsx
+++ components/repo-card.tsx
@@ -25,16 +25,18 @@ const RepoCard = ({
       : `Updated ${differenceInDays} day${differenceInDays > 1 ? "s" : ""} ago`;
   };
   return (
-    <div className="flex flex-col ">
-      <div className="flex flex-col items-start justify-center pt-4 pb-2 px-6 border-b border-[#D6D7DA]">
+    <div className="">
+      <div className="flex flex-col items-start justify-center pt-4 pb-2 px-6 border-b border-[#D6D7DA] hover:bg-[#F5F5F5]">
+        {/* top */}
         <div className="flex items-center justify-center gap-x-4">
-          <h1 className="font-sans  text-md sm:text-xl font-medium">
+          <h1 className="font-sans text-md sm:text-xl font-medium">
             {heading}
           </h1>
           <h1 className="bg-[#EFF8FF] px-2 rounded-full border-2 border-[#B2DDFF] text-xs sm:text-sm text-[#175CD2]">
             {label}
           </h1>
         </div>
+        {/* bottom */}
         <div className="flex items-center gap-x-6">
           <h1 className="flex items-center font-sans text-xs sm:text-sm test-[#181D27]">
             {language} <Dot className="text-[#1671EE] size-12" />
