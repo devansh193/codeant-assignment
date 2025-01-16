54cd97c07773392f49b9c99596eb0de0ecc1fefd
devansh <devanshverma193@gmail.com>
2025-01-17 03:50:15 +0530

    init

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,7 +9,7 @@ const Signin = () => {
       <div className="hidden lg:flex col-span-1 bg-white items-start pt-24 lg:pt-48 justify-center h-full relative">
         <div className="relative">
           <TotalEmails />
-          <div className="absolute bottom-[-120px] sm:bottom-[-160px] right-[-20px] sm:right-[-30px]">
+          <div className="absolute bottom-[-120px] sm:bottom-[-160px] right-[-20px] sm:right-[-50px]">
             <Card />
           </div>
         </div>
